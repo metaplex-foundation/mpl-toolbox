e47213d1fd7f89c771faa40842b226f0c1651de7
Loris Leiva <loris.leiva@gmail.com>
2022-12-23 16:23:44 +0000

    wip

--- infra/kinobi.cjs
+++ infra/kinobi.cjs
@@ -2,5 +2,5 @@ const { Kinobi, RenderJavaScriptVisitor } = require("@lorisleiva/kinobi");
 const idlDir = `${__dirname}/../idls`;
 const clientDir = `${__dirname}/../clients`;
 
-const kinobi = new Kinobi(`${idlDir}/candy_machine_core.json`);
+const kinobi = new Kinobi(`${idlDir}/candy_guard.json`);
 kinobi.accept(new RenderJavaScriptVisitor(`${clientDir}/js/src/generated`));
