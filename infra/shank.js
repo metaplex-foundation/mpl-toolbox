78ba7ee814504e69467615a9310dea8f6069369b
Loris Leiva <loris.leiva@gmail.com>
2022-12-23 12:41:07 +0000

    wip

---
+++ infra/shank.js
@@ -0,0 +1,14 @@
+import { generateIdl } from "@lorisleiva/shank-js";
+// import path from "path";
+// const programDir = path.join(__dirname, "..", "program");
+// const idlDir = path.join(__dirname, "idl");
+// const binaryInstallDir = path.join(__dirname, ".crates");
+
+generateIdl({
+  idlGenerator: "anchor",
+  programName: "candy_guard",
+  programId: "Guard1JwRhJkVH6XZhzoYxeBVQe872VH6QggF4BWmS9g",
+  idlDir: "../idls",
+  binaryInstallDir: "../.crates",
+  programDir: "../programs/candy-guard",
+});
