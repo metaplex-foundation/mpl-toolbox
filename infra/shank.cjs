174c63550cfe18b440647fd08bbb273a35f161cd
Loris Leiva <loris.leiva@gmail.com>
2022-12-23 15:46:14 +0000

    wip

--- infra/shank.cjs
+++ infra/shank.cjs
@@ -5,6 +5,15 @@ const programDir = `${__dirname}/../programs`;
 
 generateIdl({
   generator: "anchor",
+  programName: "candy_machine_core",
+  programId: "CndyV3LdqHUfDLmE5naZjVN8rBZz4tqhdefbAnjHG3JR",
+  idlDir,
+  binaryInstallDir,
+  programDir: `${programDir}/candy-machine-core`,
+});
+
+generateIdl({
+  generator: "anchor",
   programName: "candy_guard",
   programId: "Guard1JwRhJkVH6XZhzoYxeBVQe872VH6QggF4BWmS9g",
   idlDir,
