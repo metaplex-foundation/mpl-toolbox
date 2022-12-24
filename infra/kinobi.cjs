29720acfbb627d1c83cd5325b9bd14aeb16bac23
Loris Leiva <loris.leiva@gmail.com>
2022-12-24 12:01:46 +0000

    wip

--- infra/kinobi.cjs
+++ infra/kinobi.cjs
@@ -1,17 +1,18 @@
 const path = require("path");
 const { Kinobi, RenderJavaScriptVisitor } = require("@lorisleiva/kinobi");
 
-// IDL paths.
+// Paths.
+const clientDir = path.join(__dirname, "..", "clients");
 const idlDir = path.join(__dirname, "..", "idls");
 const idlPaths = [
   path.join(idlDir, "candy_machine_core.json"),
   path.join(idlDir, "candy_guard.json"),
 ];
 
-// Client paths.
-const clientDir = path.join(__dirname, "..", "clients");
-const jsDir = path.join(clientDir, "js", "src", "generated");
-
-// Generate clients.
+// Instantiate Kinobi.
 const kinobi = new Kinobi(idlPaths);
-kinobi.accept(new RenderJavaScriptVisitor(jsDir));
+
+// Generate JavaScript client.
+const jsDir = path.join(clientDir, "js", "src", "generated");
+const prettier = require(path.join(clientDir, "js", ".prettierrc.json"));
+kinobi.accept(new RenderJavaScriptVisitor(jsDir, { prettier }));
