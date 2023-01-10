9b53df3ab669a1c8bdc43fb7113ead1d1c0d4dbd
Loris Leiva <loris.leiva@gmail.com>
2023-01-10 17:14:37 +0000

    wip

--- infra/validator.cjs
+++ infra/validator.cjs
@@ -1,14 +1,4 @@
-const path = require("path");
 const { LOCALHOST, tmpLedgerDir } = require("@metaplex-foundation/amman");
-
-const deployDir = path.join(__dirname, "..", "target", "deploy");
-const programs = {
-  candy_guard: {
-    label: "Candy Guard",
-    programId: "Guard1JwRhJkVH6XZhzoYxeBVQe872VH6QggF4BWmS9g",
-    deployPath: path.join(deployDir, "mpl_candy_guard.so"),
-  },
-};
 
 const validator = {
   killRunningValidators: true,
@@ -19,19 +9,6 @@ const validator = {
   jsonRpcUrl: LOCALHOST,
   websocketUrl: "",
   ledgerDir: tmpLedgerDir(),
-  accountsCluster: "https://api.devnet.solana.com",
-  accounts: [
-    {
-      label: "Token Metadata Program",
-      accountId: "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
-      executable: true,
-    },
-    {
-      label: "Candy Machine Core",
-      accountId: "CndyV3LdqHUfDLmE5naZjVN8rBZz4tqhdefbAnjHG3JR",
-      executable: true,
-    },
-  ],
 };
 
 module.exports = { validator };
