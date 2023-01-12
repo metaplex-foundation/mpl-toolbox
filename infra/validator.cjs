871d01bfeff216613f2436ff140b6ecdebe2c7fa
Loris Leiva <loris.leiva@gmail.com>
2023-01-12 10:34:03 +0000

    wip

--- infra/validator.cjs
+++ infra/validator.cjs
@@ -1,14 +1,6 @@
-const { LOCALHOST, tmpLedgerDir } = require("@metaplex-foundation/amman");
-
-const validator = {
-  killRunningValidators: true,
-  programs: [],
-  commitment: "processed",
-  resetLedger: true,
-  verifyFees: false,
-  jsonRpcUrl: LOCALHOST,
-  websocketUrl: "",
-  ledgerDir: tmpLedgerDir(),
+module.exports = {
+  validator: {
+    programs: [],
+    commitment: "processed",
+  },
 };
-
-module.exports = { validator };
