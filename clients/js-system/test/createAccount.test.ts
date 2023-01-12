0aff44d2502af2c857f11d994ec166a9efba91cc
Loris Leiva <loris.leiva@gmail.com>
2023-01-12 10:08:31 +0000

    wip

--- clients/js-system/test/createAccount.test.ts
+++ clients/js-system/test/createAccount.test.ts
@@ -1,9 +1,31 @@
-import { createMetaplex, generateSigner, sol } from '@lorisleiva/js-test';
+import { createMetaplex } from '@lorisleiva/js-core';
+import { generateSigner, sol, testPlugins } from '@lorisleiva/js-test';
+import { Connection, PublicKey } from '@solana/web3.js';
 import test from 'ava';
 import { createAccount } from '../src';
 
+async function customMetaplex() {
+  const mx = createMetaplex().use(
+    testPlugins('http://127.0.0.1:8899', { commitment: 'confirmed' })
+  );
+  console.log('here', process.env.NODE_ENV);
+  const connection = (mx.rpc as any).connection as Connection;
+  const sig = await connection.requestAirdrop(
+    new PublicKey(mx.payer.publicKey.bytes),
+    100_000_000_000
+  );
+  await connection.confirmTransaction({
+    signature: sig,
+    ...(await connection.getLatestBlockhash()),
+  });
+  // await mx.rpc.airdrop(mx.payer.publicKey, sol(100), {
+  //   commitment: 'finalized',
+  // });
+  return mx;
+}
+
 test('test example', async (t) => {
-  const metaplex = await createMetaplex();
+  const metaplex = await customMetaplex();
   const account = await metaplex.rpc.getAccount(metaplex.payer.publicKey);
   console.log(account);
 
