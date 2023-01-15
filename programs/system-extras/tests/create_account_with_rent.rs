86660fc6e4ff119772ee0f9acf5f7b5eb5cfc806
Loris Leiva <loris.leiva@gmail.com>
2023-01-15 15:17:56 +0000

    wip

--- programs/system-extras/tests/create_account_with_rent.rs
+++ programs/system-extras/tests/create_account_with_rent.rs
@@ -2,7 +2,7 @@
 
 pub mod utils;
 
-use crate::utils::{get_account, program_test, send_transaction};
+use crate::utils::{airdrop, get_account, program_test, send_transaction};
 use assert_matches::*;
 use mpl_system_extras::instruction::create_account_with_rent_instruction;
 use solana_program::{
@@ -45,7 +45,44 @@ async fn test_it_creates_an_account_with_minimum_rent_from_given_space() {
     // And the right amount of lamports was transferred to the new account.
     let rent = Rent::get()?;
     assert_eq!(account.lamports, rent.minimum_balance(42));
+
+    // And the right program owner was set.
+    assert_eq!(account.owner, context.program_id);
+}
+
+#[tokio::test]
+async fn test_creating_an_account_with_rent_debit_lamports_from_the_payer() {
+    // Given a brand new account keypair and a payer with 10 SOL.
+    let mut context = program_test().start_with_context().await;
+    let new_account = Keypair::new();
+    let payer = Keypair::new();
+    airdrop(&mut context, &payer.pubkey(), 10_000_000_000).await?;
+
+    // When we create an account with rent using that payer.
+    let transaction = Transaction::new_signed_with_payer(
+        &[create_account_with_rent_instruction(
+            &payer.pubkey(),
+            &new_account.pubkey(),
+            42,
+            context.program_id,
+        )],
+        // Note that we let the context payer pay for the transaction fee
+        // so that we can assert the exact amount of lamports transferred.
+        Some(&context.payer.pubkey()),
+        &[&payer.payer],
+        context.last_blockhash,
+    );
+    send_transaction(&mut context, transaction).await?;
+
+    // Then the right amount of lamports was transferred to the new account.
+    let rent = Rent::get()?;
+    let rent_lamports = rent.minimum_balance(42);
+    let account = get_account(&mut context, &new_account.pubkey()).await;
+    assert_eq!(account.lamports, rent_lamports);
+
+    // And the same amount of lamports was debited from the payer.
+    let payer_account = get_account(&mut context, &payer.pubkey()).await;
+    assert_eq!(payer_account.lamports, 10_000_000_000 - rent_lamports);
 }
 
 // Test: Not enough funds.
-// Test: Different payer.
