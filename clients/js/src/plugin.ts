ee25cf5fffef16806e39afdd89778dc541478a93
Loris Leiva <loris.leiva@gmail.com>
2023-01-15 17:26:34 +0000

    wip

--- clients/js/src/plugin.ts
+++ clients/js/src/plugin.ts
@@ -1,25 +1,59 @@
-import { ClusterFilter, MetaplexPlugin, Program } from '@lorisleiva/js-core';
-import { getSplMemoProgram, getSplSystemProgram } from './generated';
+import {
+  ClusterFilter,
+  MetaplexPlugin,
+  Program,
+  ProgramRepositoryInterface,
+} from '@lorisleiva/js-core';
+import {
+  getMplSystemExtrasProgram,
+  getSplAssociatedTokenAccountProgram,
+  getSplMemoProgram,
+  getSplSystemProgram,
+  getSplTokenProgram,
+} from './generated';
 
 export const splSystem = (): MetaplexPlugin => ({
   install(metaplex) {
     const splSystemProgram = getSplSystemProgram(metaplex);
     metaplex.programs.add(splSystemProgram);
-    metaplex.programs.getSystem = function (clusterFilter?: ClusterFilter) {
-      return this.get(splSystemProgram.name, clusterFilter);
-    };
+    metaplex.programs.getSystem = getShortcutMethod(splSystemProgram);
 
     const splMemoProgram = getSplMemoProgram(metaplex);
     metaplex.programs.add(splMemoProgram);
-    metaplex.programs.getMemo = function (clusterFilter?: ClusterFilter) {
-      return this.get(splMemoProgram.name, clusterFilter);
-    };
+    metaplex.programs.getMemo = getShortcutMethod(splMemoProgram);
+
+    const splTokenProgram = getSplTokenProgram(metaplex);
+    metaplex.programs.add(splTokenProgram);
+    metaplex.programs.getToken = getShortcutMethod(splTokenProgram);
+
+    const splAssociatedTokenProgram =
+      getSplAssociatedTokenAccountProgram(metaplex);
+    metaplex.programs.add(splAssociatedTokenProgram);
+    metaplex.programs.getAssociatedToken = getShortcutMethod(
+      splAssociatedTokenProgram
+    );
+
+    const mplSystemExtras = getMplSystemExtrasProgram(metaplex);
+    metaplex.programs.add(mplSystemExtras);
+    metaplex.programs.getSystemExtras = getShortcutMethod(mplSystemExtras);
   },
 });
 
+function getShortcutMethod(program: Program) {
+  return function (
+    this: ProgramRepositoryInterface,
+    clusterFilter?: ClusterFilter
+  ) {
+    return this.get(program.name, clusterFilter);
+  };
+}
+
 declare module '@lorisleiva/js-core' {
   interface ProgramRepositoryInterface {
-    getSystem(clusterFilter?: ClusterFilter): Program;
+    getAssociatedToken(clusterFilter?: ClusterFilter): Program;
     getMemo(clusterFilter?: ClusterFilter): Program;
+    getSystem(clusterFilter?: ClusterFilter): Program;
+    getSystemExtras(clusterFilter?: ClusterFilter): Program;
+    getToken(clusterFilter?: ClusterFilter): Program;
   }
 }
