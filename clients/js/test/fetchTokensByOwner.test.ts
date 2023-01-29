01c65c228d4cd8c004a5d80d5b296710570eb5f2
Loris Leiva <loris.leiva@gmail.com>
2023-01-29 20:43:29 +0000

    wip

--- clients/js/test/fetchTokensByOwner.test.ts
+++ clients/js/test/fetchTokensByOwner.test.ts
@@ -4,16 +4,25 @@ import { fetchTokensByOwner } from '../src';
 import { createMetaplex, createMintAndToken } from './_setup';
 
 test('it fetches all token account owned by a given owner', async (t) => {
-  // Given an existing owner.
+  // Given an existing owner A.
   const mx = await createMetaplex();
-  const owner = generateSigner(mx).publicKey;
+  const ownerA = generateSigner(mx).publicKey;
 
-  // And
-  // const [mintA, tokenA] = await createMintAndToken(mx, { owner });
-  await createMintAndToken(mx, { owner });
+  // And two tokens owned by owner A.
+  const [, tokenA] = await createMintAndToken(mx, { owner: ownerA });
+  const [, tokenB] = await createMintAndToken(mx, { owner: ownerA });
 
-  // When
-  const foo = await fetchTokensByOwner(mx, owner);
-  console.log(foo);
-  t.pass();
+  // And a token owned by another owner B.
+  const ownerB = generateSigner(mx).publicKey;
+  await createMintAndToken(mx, { owner: ownerB });
+
+  // When we fetch all tokens owned by owner A.
+  const tokens = await fetchTokensByOwner(mx, ownerA);
+
+  // Then we only get back the two tokens owned by owner A.
+  t.is(tokens.length, 2);
+  t.deepEqual(
+    tokens.map((token) => token.publicKey),
+    [tokenA.publicKey, tokenB.publicKey]
+  );
 });
