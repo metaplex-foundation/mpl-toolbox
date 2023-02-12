68181d985fb3b9560742c81fb2f971a1ee2ba4b7
Loris Leiva <loris.leiva@gmail.com>
2023-02-12 20:06:31 +0000

    Trigger workflow

--- programs/system-extras/src/instruction.rs
+++ programs/system-extras/src/instruction.rs
@@ -79,5 +79,5 @@ pub fn transfer_all_sol_instruction(source: &Pubkey, destination: &Pubkey) -> In
 pub struct DummyChallenge {
     /// The authority that can update the challenge, normally the creator.
     pub authority: Pubkey,
-    pub bump_1: u8,
+    pub bump_2: u8,
 }
