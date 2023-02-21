d009ea70f56c91ee124b3be6017db3ecd61d544b
Loris Leiva <loris.leiva@gmail.com>
2023-02-21 17:29:16 +0000

    Update createLut.test.ts

--- clients/js/test/createLut.test.ts
+++ clients/js/test/createLut.test.ts
@@ -1,11 +1,13 @@
 import {
-  base58PublicKey,
+  generateSigner,
   PublicKey,
-  Some,
+  publicKey,
+  some,
   transactionBuilder,
 } from '@metaplex-foundation/umi-test';
 import test from 'ava';
 import {
+  AddressLookupTable,
   createLut,
   fetchAddressLookupTable,
   findAddressLookupTablePda,
@@ -13,61 +15,50 @@ import {
 import { createUmi } from './_setup';
 
 test('it can create a new empty LUT with minimum configuration', async (t) => {
-  // Given
+  // Given a recent slot.
   const umi = await createUmi();
   const recentSlot = await umi.rpc.getSlot({ commitment: 'finalized' });
 
-  // When
+  // When we create a new LUT using that slot.
   await transactionBuilder(umi)
     .add(createLut(umi, { recentSlot }))
     .sendAndConfirm();
 
-  // Then
+  // Then a new account was created with the correct data.
   const lut = findAddressLookupTablePda(umi, {
     authority: umi.identity.publicKey,
     recentSlot,
   });
   const lutAccount = await fetchAddressLookupTable(umi, lut);
-  console.log({
-    address: base58PublicKey(lut),
-    authority: base58PublicKey(umi.identity),
+  t.like(lutAccount, <AddressLookupTable>{
+    publicKey: publicKey(lutAccount),
+    authority: some(publicKey(umi.identity)),
+    addresses: [] as PublicKey[],
+    deactivationSlot: BigInt(`0x${'ff'.repeat(8)}`),
+    lastExtendedSlot: 0n,
+    lastExtendedStartIndex: 0,
   });
-  console.log(lutAccount);
-  console.log({
-    lutAddress: base58PublicKey(lutAccount),
-    lutAuthority: base58PublicKey(
-      (lutAccount.authority as Some<PublicKey>).value
-    ),
-  });
-  t.pass();
-
-  // // Then the account was created with the correct data.
-  // const mintAccount = await fetchMint(metaplex, newAccount.publicKey);
-  // const rentExemptBalance = await metaplex.rpc.getRent(getMintSize());
-  // t.like(mintAccount, <Mint>{
-  //   publicKey: newAccount.publicKey,
-  //   header: {
-  //     owner: metaplex.programs.get('splToken').publicKey,
-  //     lamports: rentExemptBalance,
-  //   },
-  //   mintAuthority: some({ ...metaplex.identity.publicKey }),
-  //   supply: 0n,
-  //   decimals: 0,
-  //   isInitialized: true,
-  //   freezeAuthority: some({ ...metaplex.identity.publicKey }),
-  // });
-
-  // // And the payer was charged for the creation of the account.
-  // const newPayerBalance = await metaplex.rpc.getBalance(
-  //   metaplex.payer.publicKey
-  // );
-  // t.true(
-  //   isEqualToAmount(
-  //     newPayerBalance,
-  //     subtractAmounts(payerBalance, rentExemptBalance),
-  //     sol(0.0001) // (tolerance) Plus a bit more for the transaction fee.
-  //   )
-  // );
 });
 
-// with a custom authority
+test('it can create a new empty LUT with a custom authority', async (t) => {
+  // Given a custom authority.
+  const umi = await createUmi();
+  const authority = generateSigner(umi);
+  const recentSlot = await umi.rpc.getSlot({ commitment: 'finalized' });
+
+  // When we create a new LUT from that authority.
+  await transactionBuilder(umi)
+    .add(createLut(umi, { recentSlot, authority }))
+    .sendAndConfirm();
+
+  // Then the created LUT has the correct authority.
+  const lut = findAddressLookupTablePda(umi, {
+    authority: authority.publicKey,
+    recentSlot,
+  });
+  const lutAccount = await fetchAddressLookupTable(umi, lut);
+  t.like(lutAccount, <AddressLookupTable>{
+    publicKey: publicKey(lutAccount),
+    authority: some(publicKey(authority)),
+  });
+});
